86f0268f18f438791b86a37165463b6a599fb885
Salmaniac Prashant <salmaniacprashant@gmail.com>
2023-08-30 17:29:06 +0530

    scripted .tf files

---
+++ vars.tf
@@ -0,0 +1,94 @@
+variable AWS_REGION {
+  default = "ap-south-1"
+}
+
+variable AMIS {
+  type = map
+  default = {
+    ap-south-1 = "ami-06f621d90fa29f6d0"
+  }
+}
+
+variable PRIV_KEY_PATH {
+  default = "vprofilekey"
+}
+
+variable PUB_KEY_PATH {
+  default = "vprofilekey.pub"
+}
+
+variable USERNAME {
+  default = "ubuntu"
+}
+
+variable MYIP {
+  default = "202.142.121.254/32"
+}
+
+variable rmquser {
+  default = "rabbit"
+}
+
+variable rmqpass {
+  default = "rabbitmqpass"
+}
+
+variable dbuser {
+  default = "admin"
+}
+
+variable dbpass {
+  default = "admin123"
+}
+
+variable dbname {
+  default = "accounts"
+}
+
+variable instance_count {
+  default = "1"
+}
+
+variable VPC_NAME {
+  default = "vprofile-VPC"
+}
+
+variable Zone1 {
+  default = "ap-south-1a"
+}
+
+variable Zone2 {
+  default = "ap-south-1b"
+}
+
+variable Zone3 {
+  default = "ap-south-1c"
+}
+
+variable VpcCIDR {
+  default = "172.21.0.0/16"
+}
+
+variable PubSub1CIDR {
+  default = "172.21.1.0/16""
+}
+
+variable PubSub2CIDR {
+  default = "172.21.2.0/24""
+}
+
+variable PubSub3CIDR {
+  default = "172.21.3.0/24""
+}
+
+variable PrivSub1CIDR {
+  default = "172.21.4.0/24""
+}
+
+variable PrivSub2CIDR {
+  default = "172.21.5.0/24""
+}
+
+variable PrivSub3CIDR {
+  default = "172.21.6.0/24""
+}
